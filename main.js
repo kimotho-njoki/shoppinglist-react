62146b9e0db6e187ca014aaec0a1ce72a159f770
kimotho-njoki <graycenjoki@gmail.com>
2017-12-08 09:14:45 +0300

    [Feature] render Routes component

--- main.js
+++ main.js
@@ -2,19 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import LoginScreen from "./components/loginscreen";
-import ViewShoppinglist from "./components/shoppinglistview";
+import Routes from "./routes";
 
-class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<ViewShoppinglist />		
-			</div>
-			);
-	}
-}
-
-ReactDOM.render(<MuiThemeProvider>
-			<App />
+ReactDOM.render(
+			<MuiThemeProvider>
+			<Routes />
 			</MuiThemeProvider>, document.getElementById('app'));
