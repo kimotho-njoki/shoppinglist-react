b6e72005a9f1fbda73e7303ee31301f36ba2ff2f
kimotho-njoki <graycenjoki@gmail.com>
2017-12-08 17:56:29 +0300

    [Chore] Include css  styles in their own file

---
+++ styling.js
@@ -0,0 +1,137 @@
+import {blue700} from 'material-ui/styles/colors';
+
+export const styles = {
+	text: {
+		borderColor: blue700,
+  	},
+  	back: {
+    	backgroundColor: blue700
+  	},
+  	p: {
+    	paddingTop: 20,
+    	color: blue700
+  	},
+  	alertOPtions: {
+    	offset: 14,
+    	position: 'bottom left',
+    	theme: 'dark',
+    	transition: 'scale'
+  	},
+  	title: {
+    	backgroundColor: blue700,
+    },
+  	slide: {
+    	paddingTop: 70,
+    	paddingLeft: 370,
+    	paddingRight: 370,
+    	display: 'flex',
+    	flexWrap: 'wrap',
+    	justifyContent: 'space-around',
+  	},
+  	regtext: {
+    	color: blue700,
+  	},
+  	link: {
+  		color: blue700,
+  		backgroundColor: "transparent",
+  		textDecoration: 'none',
+  		display: 'flex',
+  		flexWrap: 'wrap',
+  		justifyContent: 'space-around'
+  	},
+  	logtext: {
+  		paddingTop: 200,
+  		display: 'flex',
+  		flexWrap: 'wrap',
+  		justifyContent: 'space-around'
+  	},
+  	resettext: {
+    	borderColor: blue700,
+    	fontSize: 20
+  	},
+  	appbar: {
+  		backgroundColor: blue700
+  	},
+  	container: {
+  		textAlign: 'center',
+    	display: 'block',
+    	justifyContent: 'center',
+    	alignItems: 'center',
+  	},
+  	resetlink: {
+    	color: blue700,
+    	backgroundColor: "transparent",
+  	},
+  	alertOPtions: {
+    	offset: 14,
+    	position: 'bottom left',
+    	theme: 'dark',
+    	transition: 'scale'
+  	},
+  	nottext: {
+  		paddingTop: 200,
+  		display: 'flex',
+  		flexWrap: 'wrap',
+  		justifyContent: 'space-around'
+  	},
+  	textbox: {
+  		paddingTop: 20,
+  	},
+  	itemtext: {
+    	fontFamily: "verdana",
+    	fontSize: 19,
+    	color: blue700
+  	},
+  	del: {
+  		backgroundColor: redA700,
+  	},
+  	para: {
+    	borderStyle: "solid",
+    	borderWidth: "medium",
+	    borderColor: blue700,
+	    backgroundColor: blue700,
+	    paddingTop: 20,
+	    paddingBottom: 20,
+	    paddingLeft: 20,
+	    fontSize: 18,
+	    fontWeight: "bold",
+  	},
+	root: {
+	   	display: 'flex',
+	  	flexWrap: 'wrap',
+	  	justifyContent: 'space-around'
+	},
+  	itemlink: {
+	  	color: blue700,
+	  	backgroundColor: "transparent",
+	  	textDecoration: 'none'
+    },
+  	button: {
+	  	position: 'fixed',
+	  	bottom: "10%",
+	  	right: "7%",
+    },
+  	bar: {
+	  	width: "40%",
+	  	margin: '1%',
+	  	position: 'absolute',
+	  	right: '1%'
+  	},
+  	table: {
+	  	margin: '10% 25% 0 25%',
+	  	width: '50%',
+	  	display: 'block',
+	  	justifyContent: 'center',
+	  	alignItem: 'center',
+  	},
+  	gridlist: {
+	  	paddingTop: 80,
+	  	width: 900,
+	  	overflowY: 'auto',
+	},
+	listlink: {
+	  	color: blue700,
+	  	backgroundColor: "transparent",
+	  	textDecoration: 'none'
+	},
+};
