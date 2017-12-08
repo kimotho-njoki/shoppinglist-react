41ed0c9d368cb6f16d9c0daa554aa73d967c0bc1
kimotho-njoki <graycenjoki@gmail.com>
2017-12-08 09:19:00 +0300

    [Feature] add an authentication helper function

---
+++ helper.js
@@ -0,0 +1,8 @@
+export const isAuthenticated = () => {
+	const token = localStorage.getItem('token')
+	const isLoggedIn = localStorage.getItem('isLoggedIn')
+	if(isLoggedIn && token){
+		return true
+	}
+	return false
+}
