03b881391f837a8ac869c41499369d24ae42e203
kimotho-njoki <graycenjoki@gmail.com>
2017-12-19 15:50:10 +0300

    [Feature] add pagination feature when viewing shoppinglists and items

---
+++ components/misc/pagination.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import { styles } from '../../styling';
+
+const Pagination = props => (
+	<div style={styles.root}>
+
+		<FlatButton
+            style={props.prevpage === "None" ? 
+                styles.hidden : 
+                styles.flatprev}
+            label="Prev Page"
+            primary={true}
+            disabled={false}
+            onClick={() => props.handlePagination(props.prevpage)}
+        />
+        <FlatButton
+            style={props.nextpage === "None" ? 
+                styles.hidden : 
+                styles.flatnext}
+            label="Next Page"
+            primary={true}
+            disabled={false}
+            onClick={() => props.handlePagination(props.nextpage)}
+        />
+	</div>
+)
+
+export default Pagination;
